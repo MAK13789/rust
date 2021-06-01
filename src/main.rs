959904330c171675be5d3770a8c045af03b5492b
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2021-06-01 18:11:47 +0500

    :eyes:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
+    println!("{} test", 69);
+    println!("{0} is better than {1}", "Ronaldo", "Messi");
+    println!("{footballer} {goat}", footballer = "messi", goat = "ronaldo");
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
 }
